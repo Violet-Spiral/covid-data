3df909e45240b76d5f027508b3537ea31e849cd7
laylar <layla.rowen@gmail.com>
2021-01-05 12:00:48 -0800

    imported df for country dropdown, started state dropdown

--- app.py
+++ app.py
@@ -4,28 +4,58 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+import pandas as pd
+
+DATA_URL = 'https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_latest.csv'
+full_df = pd.read_csv(DATA_URL,
+                      parse_dates=['Date'],
+                      encoding="ISO-8859-1",
+                      dtype={"RegionName": str,
+                             "RegionCode": str,
+                             "CountryName": str,
+                             "CountryCode": str},
+                      error_bad_lines=False)
+unique_countries = full_df['CountryName'].unique()
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
+unique_states = []
 
 app.layout = html.Div([
     html.H2('Hello World'),
+    html.H4('Country'),
     dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
+        id='dropdown_country',
+        options=[{'label': i, 'value': i} for i in unique_countries],
+        value='Select a country'
     ),
-    html.Div(id='display-value')
+    html.Div(id='country_value'),
+    html.H4('State'),
+    dcc.Dropdown(
+        id='dropdown_state',
+        options=[{'label': i, 'value': i} for i in unique_states],
+        value='Select a state'
+    ),
+    html.Div(id='state_value')
 ])
 
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+@app.callback(dash.dependencies.Output('country_value', 'children'),
+              [dash.dependencies.Input('dropdown_country', 'value')])
+def display_value(country_value):
+    global unique_states
+    unique_states = full_df[full_df['CountryName']
+                            == country_value]['RegionName'].unique()
+    return unique_states
 
+
+# @app.callback(dash.dependencies.Output('state_value', 'children'),
+#             [dash.dependencies.Input('dropdown_country', 'value')])
+# def state_list(country):
+#     return full_df[full_df['CountryName'] == country_value].unique()
 
 if __name__ == '__main__':
     app.run_server(debug=True)
